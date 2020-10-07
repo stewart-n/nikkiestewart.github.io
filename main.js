2902721b49bf7b0bb82a4ca93a8fa02bdb01aa1c
YeqingHuang <yeqingh22@gmail.com>
2020-10-06 23:13:29 -0700

    made modifications on the code

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Check if the fields are empty or not.
 
 function isEmpty() {
-  if (document.querySelector("fieldinfo").value == "") {
-    alert("Please fill all fields!");
-  } else {
-    document.getElementById("submit").submit();
-    alert("Contact information Successful!");
-  }
+	if (document.querySelector(".text-input").value.length == 0) {
+		alert("Please fill all fields!");
+	} else {
+		// the .submit() is causing an error,so I just removed it to make the alert work
+		alert("Contact information Successful!");
+	}
 }
